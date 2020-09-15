fdd6af2786cf4e7d061a828d3e4d9a59e1ea59ff
Franck Bianciotto <1966388@cstj.qc.ca>
2020-09-15 09:05:42 -0400

    Ajoutez des fichiers projet.

---
+++ ProjectFranckBianciotto/Location.cpp
@@ -0,0 +1,18 @@
+// But : Écrire un programme qui dit combien quelqu'un doit dépense pour faire un voyage en automobile louer.
+// Auteur : Franck, Bianciotto
+// Date : 2020-09-15
+
+
+#include<iostream>
+
+void main()
+{
+
+
+
+
+
+
+
+
+}
